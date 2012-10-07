e6d66bec5389ac440c5bcf0b432f44274271fbdd
Han Xu <hgcrpd@gmail.com>
2012-10-07 04:34:22 -0700

    initial commit of rails tutorial

---
+++ ruby/rails/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ef299f0c849a9a5ac660f520cc4fa20d2ac0495e874fa310c9c4b43116643b3a70686059e05b985582bdce6f8f10f09e2edfadeab51e9d19203e9ff840665d3a'
